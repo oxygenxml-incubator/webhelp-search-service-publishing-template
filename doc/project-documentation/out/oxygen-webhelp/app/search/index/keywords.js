744c452493561eb4061b5077040eae2812256321
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-14 01:31:38 -0700

    Delete doc/project-documentation/out directory

--- doc/project-documentation/out/oxygen-webhelp/app/search/index/keywords.js
+++
@@ -1,8 +0,0 @@
-const keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Publishing",p:["p1"]},{w:"your",p:["p1"]},{w:"documentation",p:["p1"]},{w:"with",p:["p1"]},{w:"WebHelp",p:["p1"]},{w:"Responsive",p:["p1"]},{w:"using",p:["p1"]},{w:"GitHub",p:["p1"]},{w:"Actions",p:["p1"]}];
-const ph={};
-ph["p0"]=[0, 1, 2];
-ph["p1"]=[3, 4, 5, 6, 7, 8, 9, 10, 11];
-const keywordsInfo = {
-    keywords: keywords,
-    ph: ph
-}
--- doc/project-documentation/out/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,2 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-const linkToParent = {1:-1};
