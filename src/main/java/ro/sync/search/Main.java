6269df1908e9cd6ed66fca20806231375826ce42
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-14 16:33:33 +0300

    #6 Created Crawler object

---
+++ src/main/java/ro/sync/search/Main.java
@@ -0,0 +1,15 @@
+package ro.sync.search;
+
+/**
+ * The main class of the search service where all the necessary methods are
+ * called
+ * 
+ * @author Artiom Bozieac
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		Crawler crawler = new Crawler("https://snazzy-dusk-c2b3e5.netlify.app");
+		crawler.crawl();
+	}
+}
