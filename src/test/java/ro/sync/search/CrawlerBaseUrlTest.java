a01af690bff32fb2b737c484fd79ac8cc6061928
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-15 11:57:35 +0300

    #6 Added tests for Crawler.getBaseUrl()

---
+++ src/test/java/ro/sync/search/CrawlerBaseUrlTest.java
@@ -0,0 +1,29 @@
+package ro.sync.search;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.javatuples.Pair;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CrawlerBaseUrlTest {
+	private static Crawler crawler;
+
+	private static Stream<Pair<String, String>> provideParameters() {
+		return Stream.of(
+				new Pair<String, String>("https://www.google.com/search?q=something", "https://www.google.com"),
+				new Pair<String, String>("https://translate.google.com/?sl=en&tl=ru&text=something&op=translate",
+						"https://translate.google.com"),
+				new Pair<String, String>("https://www.news.com.au/lifestyle/real-life/news-life",
+						"https://www.news.com.au"));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideParameters")
+	void testBaseUrls(final Pair<String, String> urls) {
+		crawler = new Crawler(urls.getValue0());
+		assertEquals(crawler.getBaseUrl().toString(), urls.getValue1());
+	}
+}
