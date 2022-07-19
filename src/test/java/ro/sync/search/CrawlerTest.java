89116873c0a0e5f1c794ffca0e64928cba31a122
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-19 10:43:16 +0300

    #6 Added new tests with local files

--- src/test/java/ro/sync/search/CrawlerTest.java
+++ src/test/java/ro/sync/search/CrawlerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,38 +14,14 @@ class CrawlerTest {
 	private static Crawler crawler;
 
 	@Test
-	void crawlSyncro() throws MalformedURLException {
-		crawler = new Crawler("https://sync.ro", "https://sync.ro");
-		crawler.crawl();
-		List<URL> expected = new ArrayList<>() {
-			{
-				add(new URL("https://sync.ro/index.html"));
-				add(new URL("https://sync.ro/company.html"));
-				add(new URL("https://sync.ro/products.html"));
-				add(new URL("https://sync.ro/jobs.html"));
-				add(new URL("https://sync.ro/impact.html"));
-				add(new URL("https://sync.ro/contact.html"));
-				add(new URL("https://sync.ro/internship.html"));
-				add(new URL("https://sync.ro/terms_of_use.html"));
-				add(new URL("https://sync.ro/privacy_policy.html"));
-			}
-		};
-
-		assertEquals(crawler.getVisitedUrls(), expected);
-	}
-
-	@Test
 	void recursionTest() throws MalformedURLException {
-		crawler = new Crawler("https://snazzy-dusk-c2b3e5.netlify.app", "https://snazzy-dusk-c2b3e5.netlify.app");
+		crawler = new Crawler(Path.of("target/test-classes/recursion/index.html").toUri().toURL().toString(),
+				Path.of("target/test-classes/recursion/").toUri().toURL().toString());
 		crawler.crawl();
 		List<URL> expected = new ArrayList<>() {
 			{
-				add(new URL("https://snazzy-dusk-c2b3e5.netlify.app/index.html"));
-				add(new URL(
-						"https://snazzy-dusk-c2b3e5.netlify.app/topics/publishing_your_documentation_with_webhelp_responsive_using_github_actions.html"));
-				add(new URL("https://snazzy-dusk-c2b3e5.netlify.app/../index.html"));
-				add(new URL(
-						"https://snazzy-dusk-c2b3e5.netlify.app/../topics/publishing_your_documentation_with_webhelp_responsive_using_github_actions.html"));
+				add(new URL(crawler.getBaseUrl() + "recursion.html"));
+				add(new URL(crawler.getBaseUrl() + "index.html"));
 			}
 		};
 
@@ -52,10 +29,26 @@ class CrawlerTest {
 	}
 
 	@Test
-	void crawlWebsiteWithoutHtmlReferences() {
-		crawler = new Crawler("https://www.w3schools.com/html/html_styles.asp", "https://www.w3schools.com/html/");
+	void multipleLinesTest() throws MalformedURLException {
+		crawler = new Crawler(Path.of("target/test-classes/multipleLines/index.html").toUri().toURL().toString(),
+				Path.of("target/test-classes/multipleLines/").toUri().toURL().toString());
+		crawler.crawl();
+		List<URL> expected = new ArrayList<>() {
+			{
+				add(new URL(crawler.getBaseUrl() + "other.html"));
+			}
+		};
+
+		assertEquals(crawler.getVisitedUrls(), expected);
+	}
+
+	@Test
+	void relativeHrefTest() throws MalformedURLException {
+		crawler = new Crawler(Path.of("target/test-classes/relativeHref/index/index.html").toUri().toURL().toString(),
+				Path.of("target/test-classes/relativeHref/index/").toUri().toURL().toString());
 		crawler.crawl();
 		List<URL> expected = new ArrayList<>();
+
 		assertEquals(crawler.getVisitedUrls(), expected);
 	}
 }
