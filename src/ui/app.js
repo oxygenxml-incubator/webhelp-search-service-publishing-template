5f5b6016ccad08a01e5f10a55c3949abe669b3fa
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-25 14:43:23 +0300

    Format the code

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,36 +1,36 @@
 // Replace with your own values
 const searchClient = algoliasearch(
-  'KLFWXPOEHY',
-  'ff20cb14577be8b5eab7ead0857dd573' // search only API key, not admin API key
+	'KLFWXPOEHY',
+	'ff20cb14577be8b5eab7ead0857dd573' // search only API key, not admin API key
 )
 
 const search = instantsearch({
-  indexName: 'Syncro pages',
-  searchClient,
-  routing: true,
+	indexName: 'Syncro pages',
+	searchClient,
+	routing: true,
 })
 
 search.addWidgets([
-  instantsearch.widgets.configure({
-    hitsPerPage: 10,
-  }),
+	instantsearch.widgets.configure({
+		hitsPerPage: 10,
+	}),
 ])
 
 search.addWidgets([
-  instantsearch.widgets.searchBox({
-    container: '#search-box',
-    placeholder: 'Search for contacts',
-  }),
+	instantsearch.widgets.searchBox({
+		container: '#search-box',
+		placeholder: 'Search for contacts',
+	}),
 ])
 
 search.addWidgets([
-  instantsearch.widgets.hits({
-    container: '#hits',
-    templates: {
-      item: document.getElementById('hit-template').innerHTML,
-      empty: `We didn't find any results for the search <em>"{{query}}"</em>`,
-    },
-  }),
+	instantsearch.widgets.hits({
+		container: '#hits',
+		templates: {
+			item: document.getElementById('hit-template').innerHTML,
+			empty: `We didn't find any results for the search <em>"{{query}}"</em>`,
+		},
+	}),
 ])
 
 search.start()
