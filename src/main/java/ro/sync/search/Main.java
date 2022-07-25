11cc927ac59ccb689c78f42801bd32cfdf4f1d1b
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-25 13:59:35 +0300

    #5 Add example

--- src/main/java/ro/sync/search/Main.java
+++ src/main/java/ro/sync/search/Main.java
@@ -1,9 +1,17 @@
 package ro.sync.search;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.algolia.search.DefaultSearchClient;
+import com.algolia.search.SearchClient;
+import com.algolia.search.SearchIndex;
+import com.algolia.search.models.indexing.Query;
 
 /**
  * The main class of the search service where all the necessary methods are
@@ -20,14 +28,24 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Crawler crawler;
+		Crawler crawler = null;
 		try {
-			crawler = new Crawler("https://www.w3schools.com", "https://www.w3schools.com", false);
+			crawler = new Crawler("https://www.sync.ro", "https://www.sync.ro", false);
 			crawler.crawl();
 			JsonHandler jsonHandler = new JsonHandler(crawler.getCrawledPages());
-			logger.info(jsonHandler.getJson());
 		} catch (MalformedURLException e) {
 			logger.error("An error occured when initializing URLs!");
 		}
+
+		try (SearchClient client = DefaultSearchClient.create("KLFWXPOEHY", "87c939ac9269c88a17beeaacca28567a");) {
+			SearchIndex<Page> index = client.initIndex("Syncro pages", Page.class);
+			index.saveObjects(crawler.getCrawledPages());
+
+			logger.info(index.search(new Query("Internship")).toString());
+		} catch (Exception e) {
+			logger.error("An error occured when performing Algolia API calls!");
+			e.printStackTrace();
+		}
+
 	}
 }
