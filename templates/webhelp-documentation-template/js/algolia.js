c501a9edf1f67afaa477cc93dc4397b88c3af365
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-07-28 04:32:33 -0700

    #5 Change appId and add short description

--- templates/webhelp-documentation-template/js/algolia.js
+++ templates/webhelp-documentation-template/js/algolia.js
@@ -1,4 +1,3 @@
-// Connect and authenticate with your Algolia app
 const searchClient = algoliasearch('KLFWXPOEHY', '8e4e1e3ae2fc1931b0a5f5d3c8f7544d');
 const searchInstance = instantsearch({
 	indexName: 'webhelp-search-service-publishing-template',
@@ -13,7 +12,7 @@ const algoliaSearch = {
             const meta = new WebHelpAPI.SearchMeta('Algolia', obj.nbHits, obj.page, obj.hitsPerPage, obj.nbPages, query, false, false, false, false, false, false);
             
             const documents = obj.hits.map(it => {
-                return new WebHelpAPI.SearchDocument(it.objectID, it.title, "", [], 0, [], []);
+                return new WebHelpAPI.SearchDocument(it.objectID, it.title, it.shortDescription, [], 0, [], []);
             });
 
             successHandler(new WebHelpAPI.SearchResult(meta, documents));
@@ -31,7 +30,7 @@ const algoliaSearch = {
         const meta = new WebHelpAPI.SearchMeta('Algolia', obj.nbHits, obj.page, obj.hitsPerPage, obj.nbPages, query, false, false, false, false, false, false);
         
         const documents = obj.hits.map(it => {
-            return new WebHelpAPI.SearchDocument(it.objectID, it.title, "", [], 0, [], []);
+            return new WebHelpAPI.SearchDocument(it.objectID, it.title, it.shortDescription, [], 0, [], []);
         });
 
         successHandler(new WebHelpAPI.SearchResult(meta, documents));
