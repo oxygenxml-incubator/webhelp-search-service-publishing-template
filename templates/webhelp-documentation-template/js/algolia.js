9111f345843733d839f082029f0994885a604e0f
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-01 15:07:51 +0300

    #27 Replace short description with snippet

--- templates/webhelp-documentation-template/js/algolia.js
+++ templates/webhelp-documentation-template/js/algolia.js
@@ -1,18 +1,20 @@
 const searchClient = algoliasearch('40V95VH5YU', '8e4e1e3ae2fc1931b0a5f5d3c8f7544d');
-const searchInstance = instantsearch({
+
+const search = instantsearch({
 	indexName: 'webhelp-search-service-publishing-template',
-	searchClient,
+    searchClient,
 	routing: true,
 })
 
 const algoliaSearch = {
     performSearchOperation(query, successHandler, errorHandler){
-        const result = searchClient.initIndex(searchInstance.indexName).search(query);
+        const result = searchClient.initIndex(search.indexName).search(query);
+        console.log(result);
         result.then(obj => {
             const meta = new WebHelpAPI.SearchMeta('Algolia', obj.nbHits, obj.page, obj.hitsPerPage, obj.nbPages, query, false, false, false, false, false, false);
             
             const documents = obj.hits.map(it => {
-                return new WebHelpAPI.SearchDocument(it.objectID, it.title, it.shortDescription, [], 0, [], []);
+                return new WebHelpAPI.SearchDocument(it.objectID, it.title, it._snippetResult.contents.value, [], 0, [], (it._highlightResult.title.matchedWords).concat(it._highlightResult.shortDescription.matchedWords, it._highlightResult.contents.matchedWords));
             });
 
             successHandler(new WebHelpAPI.SearchResult(meta, documents));
@@ -22,7 +24,7 @@ const algoliaSearch = {
     },
 
     onPageChangedHandler(pageToShow, query, successHandler, searchFailed){
-       const result = searchClient.initIndex(searchInstance.indexName).search(query, {
+       const result = searchClient.initIndex(search.indexName).search(query, {
         page: pageToShow,
       });
 
@@ -30,7 +32,7 @@ const algoliaSearch = {
         const meta = new WebHelpAPI.SearchMeta('Algolia', obj.nbHits, obj.page, obj.hitsPerPage, obj.nbPages, query, false, false, false, false, false, false);
         
         const documents = obj.hits.map(it => {
-            return new WebHelpAPI.SearchDocument(it.objectID, it.title, it.shortDescription, [], 0, [], []);
+            return new WebHelpAPI.SearchDocument(it.objectID, it.title, it._snippetResult.contents.value, [], 0, [], (it._highlightResult.title.matchedWords).concat(it._highlightResult.shortDescription.matchedWords, it._highlightResult.contents.matchedWords));
         });
 
         successHandler(new WebHelpAPI.SearchResult(meta, documents));
