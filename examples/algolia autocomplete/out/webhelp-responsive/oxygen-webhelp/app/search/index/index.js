e7e53cfd7621db4c95553deb02bddf54dd958723
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-04 17:49:59 +0300

    #28 Add example

---
+++ examples/algolia autocomplete/out/webhelp-responsive/oxygen-webhelp/app/search/index/index.js
@@ -0,0 +1,24 @@
+const words = {
+    ...index1,
+    ...index2,
+    ...index3,
+}
+const index = {
+	/**
+     * The object with indexed words.
+     *
+     * {"word" : "topicID*score, topicID*score"}
+     */
+    w : words,
+    /**
+     * Auto generated list of analyzer stop words that must be ignored by search.
+     */
+    stopWords : stopwords,
+
+    /**
+     * File info list.
+     */
+    fil : htmlFileInfoList,
+
+    link2parent : linkToParent
+}
