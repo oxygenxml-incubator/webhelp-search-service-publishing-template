20a9227fd58834ee93459e4d070a564a9ce8e7aa
radu-pisoi <radupisoi23@gmail.com>
2022-08-04 10:29:19 +0300

    Small change

--- src/main/java/ro/sync/search/AlgoliaClient.java
+++ src/main/java/ro/sync/search/AlgoliaClient.java
@@ -37,6 +37,7 @@ public class AlgoliaClient {
 	 * Client that performs operations such as indices management.
 	 */
 	private SearchClient client;
+	
 	/**
 	 * Index that stores the current index performing actions on;
 	 */
