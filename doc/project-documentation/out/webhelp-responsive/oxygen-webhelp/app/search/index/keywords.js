942dc0420b4e664f9929dc0d8e6ef34e9d032d44
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-05 12:17:56 -0700

    Delete doc/project-documentation/out/webhelp-responsive directory

--- doc/project-documentation/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++
@@ -1,9 +0,0 @@
-const keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Crawling",p:["p1"]},{w:"data",p:["p1"]},{w:"and",p:["p1"]},{w:"use",p:["p1"]},{w:"it",p:["p1"]},{w:"with",p:["p1","p2"]},{w:"Algolia",p:["p1"]},{w:"Publishing",p:["p2"]},{w:"your",p:["p2"]},{w:"documentation",p:["p2"]},{w:"WebHelp",p:["p2"]},{w:"Responsive",p:["p2"]},{w:"using",p:["p2"]},{w:"GitHub",p:["p2"]},{w:"Actions",p:["p2"]}];
-const ph={};
-ph["p0"]=[0, 1, 2];
-ph["p1"]=[3, 4, 5, 6, 7, 8, 9];
-ph["p2"]=[10, 11, 12, 8, 13, 14, 15, 16, 17];
-const keywordsInfo = {
-    keywords: keywords,
-    ph: ph
-}
