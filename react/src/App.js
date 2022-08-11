c79faac7fa4bf52f73343cf61dae566ee3093f35
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-11 10:52:37 +0300

    #40 Add search button

--- react/src/App.js
+++ react/src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import SearchInput from './components/SearchInput.jsx';
-import SearchButton from './components/SearchButton.jsx';
+import React from "react";
+import SearchInput from "./components/SearchInput.jsx";
+import SearchButton from "./components/SearchButton.jsx";
 
 const App = () => {
-    return (
+  return (
     <div className="search-page">
-        <div className="search-container">
-            <div className="search-field">
-                {/* <SearchInput />
-                <SearchButton /> */}
-            </div>
+      <div className="search-container">
+        <div className="search-field">
+            <SearchInput />
+            <SearchButton /> 
         </div>
-        <div className="results-container">
-                
-        </div>
+      </div>
+      <div className="results-container"></div>
     </div>
-);};
+  );
+};
 
 export default App;
--- react/src/components/SearchButton.jsx
+++ react/src/components/SearchButton.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const SearchButton = () => {
-    return (<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />);
+    return (<button><FontAwesomeIcon icon={faMagnifyingGlass} /></button>);
 }
 
 export default SearchButton;
