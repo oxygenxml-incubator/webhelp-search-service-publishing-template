8511db44eaf2f4dccfb65d28f026149dbead0379
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-11 13:50:27 +0300

    #40 Create HTML structure

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import SearchInput from "./components/SearchInput.jsx";
-import SearchButton from "./components/SearchButton.jsx";
+import SearchComponent from "./components/SearchComponent.jsx";
+import SearchInformation from "./components/SearchInformation.jsx";
+import HitsList from "./components/HitsList.jsx";
+import HitsItem from "./components/hitsItem.jsx";
 
 const App = () => {
   return (
     <div className="search-page">
       <div className="search-container">
-        <div className="search-field">
-            <SearchInput />
-            <SearchButton /> 
-        </div>
+        <SearchComponent />
       </div>
-      <div className="results-container"></div>
+      <div className="results-container">
+        <SearchInformation hitsInformation="0 documents found on query." pageInformation="Page 0/0" />
+        <HitsList items={[<HitsItem title="Title" description="Something" />]} />
+      </div>
     </div>
   );
 };
