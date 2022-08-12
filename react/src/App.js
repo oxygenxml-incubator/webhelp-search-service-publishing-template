d5185893bd6a5fbd49f9936b83346452e2f6b2a4
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-12 10:00:22 +0300

    #40 Check if query is empty

--- react/src/App.js
+++ react/src/App.js
@@ -5,22 +5,32 @@ import HitsList from "./components/HitsList.jsx";
 import HitsItem from "./components/hitsItem.jsx";
 import algoliasearch from "algoliasearch/lite";
 
+// Create an Algolia SearchClient using App key and Search-only API key.
 const searchClient = algoliasearch(
   "KLFWXPOEHY",
   "ff20cb14577be8b5eab7ead0857dd573"
 );
+
+// Create a Search Instance with needed index.
 const searchInstance = searchClient.initIndex(
   "webhelp-search-service-publishing-template"
 );
 
 const App = () => {
+  // Create a state variable that stores the search result.
   const [result, setResult] = useState({ hits: [], nbHits: 0, nbPages: 0, page: 0, query: "" });
+  // Create a state variable that stores the search term.
   const [searchTerm, setSearchTerm] = useState("");
 
+  // Fetch the Algolia response based on written search term.
   const search = async (searchTerm) => {
-    const result = await searchInstance.search(searchTerm);
+    let response = { hits: [], nbHits: 0, nbPages: 0, page: 0, query: "" };
 
-    setResult(result);
+    // If search term is not empty then get the results.
+    if(searchTerm.localeCompare("") !== 0)
+      response = await searchInstance.search(searchTerm);
+    
+    setResult(response);
   };
 
   return (
@@ -44,10 +54,10 @@ const App = () => {
           items={ Object.keys(result.hits).length > 0 ? (result.hits.map((item) => {
             return (
               <HitsItem
-                key={item.objectId}
+                key={"objectID" in item ? item.objectID : item.toString()}
                 title={item.title}
                 description={item.shortDescription}
-                url={item.objectId}
+                url={item.objectID}
               />
             );
           })) : []}
