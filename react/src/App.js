b8bd5610ab28bebe77b9e2a9d442ad9b24e1517b
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-08-26 10:24:14 +0300

    #51 Add styles

--- react/src/App.js
+++ react/src/App.js
@@ -46,7 +46,7 @@ const App = () => {
         hitsPerPage: 10,
         page: page,
         restrictSearchableAttributes: searchableAttributes,
-        facetFilters: facetFilters
+        facetFilters: facetFilters,
       });
 
       setResult(response);
